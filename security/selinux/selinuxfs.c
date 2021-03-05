a534aed10eca26bb5d73c9cbbbe649cad633b2e4
Francesco <60934657+FraSharp@users.noreply.github.com>
2021-03-05 09:27:03 +0100

    idk

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -108,7 +108,7 @@ static ssize_t sel_read_enforce(struct file *filp, char __user *buf,
 	char tmpbuf[TMPBUFLEN];
 	ssize_t length;
 
-	length = scnprintf(tmpbuf, TMPBUFLEN, "%d", selinux_enforcing);
+	length = scnprintf(tmpbuf, TMPBUFLEN, "%d", user_selinux_enforcing);
 	return simple_read_from_buffer(buf, count, ppos, tmpbuf, length);
 }
 
